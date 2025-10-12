72bb7de3c769cb09832254458571ceb38e02e768
Omkarlaxmansurvase <omkarsurvase2104@gmail.com>
2025-10-12 14:44:14 +0530

    formated chats

--- Frontend/src/Chat.jsx
+++ Frontend/src/Chat.jsx
@@ -1,24 +1,56 @@
 import "./Chat.css";
 import React, { useContext } from "react";
 import { MyContext } from "./MyContext";
-// import ReactMarkdown from "react-markdown";
-// import rehypeHighlight from "rehype-highlight";
-// import "highlight.js/styles/github-dark.css";
+import ReactMarkdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
 
 function Chat(){
-    const {newChat,prevChats}=useContext(MyContext);
+    const {newChat,prevChats,reply}=useContext(MyContext);
+    const [LatestReply, setLatestReply] = useState(null);
+
+    useEffect(()=>{
+        if(!prevChats.length) return;
+        const content = reply.split("")//individual words
+        let idx =0;
+        const interval = setInterval(()=>{
+            setLatestReply(content.slice(0,idx+1).join(""));
+
+            idx++;
+            if(idx>=content.length) clearInterval(interval);
+        },40
+    )
+    },[prevChats,reply])
     return(
         <>
         {newChat && <h1>Start a New Chat!</h1>}
         <div className="chat">
             {
-                prevChats.map((chat,index)=>
+                prevChats.slice(0,-1)((chat,index)=>
                 <div className={chat.roll==="user"?"userDiv":"gptDiv"} key={idx}>
                     {
                         chat.role==="user"?
                         <p className="userMesaage">{chat.content}</p> :
-                        <p className="gptMesaage">{chat.content}</p>
+                        <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{chat.content}</ReactMarkdown>
                     }
+                    {
+                    prevChats.length > 0  && (
+                        <>
+                            {
+                                LatestReply === null ? (
+                                    <div className="gptDiv" key={"non-typing"} >
+                                    <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{prevChats[prevChats.length-1].content}</ReactMarkdown>
+                                </div>
+                                ) : (
+                                    <div className="gptDiv" key={"typing"} >
+                                     <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{latestReply}</ReactMarkdown>
+                                </div>
+                                )
+
+                            }
+                        </>
+                    )
+                }
 
                 </div>
                 )
